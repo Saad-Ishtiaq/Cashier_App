059c5696b17f697eeda7597d9b9f899324468707
Saad Ishtiaq <saad-ishtiaq@outlook.com>
2025-03-26 18:04:46 +0100

    Add ProductLoader implementation with tests and product data file - Wrote tests for ProductLoader - Added product data in 'data/products.csv'
    - Implemented ProductLoader class to load product data from CSV

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,3 +2,5 @@ require 'minitest/autorun'
 require 'minitest/reporters'
 require 'mocha/minitest'
 Minitest::Reporters.use! Minitest::Reporters::SpecReporter.new
+
+require_relative '../app/loaders/product_loader'
